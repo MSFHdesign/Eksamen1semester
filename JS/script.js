81cf7fcdc833f519e001c19e09a28b118ac639a9
MSFHdesign <90240516+MSFHdesign@users.noreply.github.com>
2021-12-09 14:52:49 +0100

    Add files via upload
    Med MTB map

---
+++ JS/script.js
@@ -0,0 +1,20 @@
+function sadle() {
+    document.getElementById("teasere").textContent = " Du sidder sjældent, det er derfor vigtigt at sadlen er indstillet helt korrekt. Der er mange faktorer der har indflydelse på hvordan du sidder bedst på din MTB. Der kan med fordel læses vores fulde artikel her. ";
+  }
+
+  function bremser() {
+    document.getElementById("teasere").textContent = " De fleste MTB kører med skivebremser da dette giver den mest effektive bremseegenskab når du kører i skov, og findes i to typer. Mekanisk og Hydrauliske bremser. Læs hele guiden på bremser her. ";
+  }
+
+  function gear() {
+    document.getElementById("teasere").textContent = " Der findes to langt gear. Indvendige gear. Denne type på en MTB i skoven er næsten ubrugelig. Vælg kun dette hvis du er sikker på du kun skal bruge den på veje eller stier.Udvendige gear. Kræver mere vedligeholde, men er det rette valg til alt terræn.";
+  }
+
+  function pedal() {
+    document.getElementById("teasere").textContent = " Selvom de kan virke lidt ubetydelige for dig som nybegynder, så er pedaler ikke noget du bare skal springe let og elegant henover. Pedaler er nemlig altafgørende, for det er dem, du driver din nye mountainbike frem med. Læs mere her. ";
+  }
+
+
+  function stel() {
+    document.getElementById("teasere").textContent = " For at få maksimal komfort og kraftoverførsel er det vigtigt, at din nye MTB er i den helt rigtige størrelse. Her kan du vælge mellem Aluminium og Carbon. Læs mere her";
+  }
