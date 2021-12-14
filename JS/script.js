6415804518ac10d79c066d06ad64f513aef8c682
MSFHdesign <90240516+MSFHdesign@users.noreply.github.com>
2021-12-14 16:16:39 +0100

    Add files via upload

--- JS/script.js
+++ JS/script.js
@@ -3,7 +3,7 @@ function sadle() {
 }
 
 function bremser() {
-  document.getElementById("teasere").innerHTML = "<u><b>Bremser</b></u> <br> De fleste MTB kører med skivebremser da dette giver den mest effektive bremseegenskab når du kører i skov, og findes i to typer. Mekanisk og Hydrauliske bremser. Læs hele guiden på bremser her. ";
+  document.getElementById("teasere").innerHTML = "<u><b>Bremser</b></u> <br> De fleste MTB kører med skivebremser da dette giver den mest effektive bremseegenskab når du kører i skov, og findes i to typer. Mekanisk og Hydrauliske bremser. ";
 }
 
 function gear() {
@@ -14,30 +14,19 @@ function pedal() {
   document.getElementById("teasere").innerHTML = "<u><b>Pedal</b></u> <br> Selvom de kan virke lidt ubetydelige for dig som nybegynder, så er pedaler ikke noget du bare skal springe let og elegant henover. Pedaler er nemlig altafgørende, for det er dem, du driver din mountainbike frem med. Læs mere her. ";
 }
 
+function suspension() { 
+  document.getElementById("teasere").innerHTML = "<u><b>Suspension</b></u> <br> Der findes to typer suspension, suspension og full suspension. ";
+}
+ 
+
 
 function stel() {
-  document.getElementById("teasere").innerHTML = "<u><b>Stel</b></u> <br> For at få maksimal komfort og kraftoverførsel er det vigtigt, at din nye MTB er i den helt rigtige størrelse, læs vores størrelsesguide.<br> Stellet kommet i to materialer. Aluminium og Carbon. Læs mere her";
+  document.getElementById("teasere").innerHTML = "<u><b>Stel</b></u> <br> For at få maksimal komfort og kraftoverførsel er det vigtigt, at din nye MTB er i den helt rigtige størrelse, læs vores størrelsesguide.<br> Stellet kommet i to materialer. Aluminium og Carbon.";
+}
+function daek() {
+  document.getElementById("teasere").innerHTML = "<u><b>Dæk</b></u> <br> lorem!!!.";
 }
 
-function next1() {
-  document.getElementById("budget").style.display ="none";
-  document.getElementById("q1").style.display = "block";
-
-}
-function next2(){
-  document.getElementById("q1").style.display ="none";
-  document.getElementById("q2").style.display = "block";
-}
-
-
-function next3(){
-  document.getElementById("q2").style.display ="none";
-  document.getElementById("q3").style.display = "block";
-
-}
-
-function next4(){
-  document.getElementById("q3").style.display ="none";
-  document.getElementById("q4").style.display = "block";
-
+function tomspace() {
+  document.getElementById("teasere").innerHTML = "Klik på cyklens dele og få mere information";
 }
